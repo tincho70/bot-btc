12217dc1dd7f09fde4baa6851c3c1ce3e7cbd14c
tincho70 <mmiraglia@gmail.com>
2024-03-08 10:43:31 -0300

    Production scripts fixed

--- scripts/prod.ps1
+++ scripts/prod.ps1
@@ -3,5 +3,5 @@ $j = Get-Content package.json | ConvertFrom-Json
 $TAG = $j.version
 
 docker compose -f docker-compose.prod.yml build
-docker tag tincho70/bot-btc:$TAG tincho70/bot-btc:latest
+docker tag tincho70/bot-btc:latest tincho70/bot-btc:$TAG
 docker push tincho70/bot-btc -a
--- scripts/prod.sh
+++ scripts/prod.sh
@@ -8,5 +8,5 @@ cd "$(readlink -e -- "${0%/*}/.." || true)"
 export TAG=$(jq -r '.version' package.json)
 
 docker compose -f docker-compose.prod.yml build
-docker tag tincho70/bot-btc:$TAG tincho70/bot-btc:latest
+docker tag tincho70/bot-btc:latest tincho70/bot-btc:$TAG
 docker push tincho70/bot-btc -a
