df4438d81126286f8b74ae08527e052605083e98
tincho70 <mmiraglia@gmail.com>
2024-03-14 16:29:41 -0300

    API debug

--- src/service/coinbase/coinbase.ts
+++ src/service/coinbase/coinbase.ts
@@ -2,8 +2,13 @@ import { Client } from "discord.js";
 import WebSocket from "ws";
 import { SocksProxyAgent } from "socks-proxy-agent";
 import { CoinbaseMessage } from "./coinbase.d";
+import { logger } from "../../helpers";
+import { Debugger } from "debug";
 
 const watchPrice = async (client: Client) => {
+  const log: Debugger = logger.extend("watchPrice");
+  const debug: Debugger = log.extend("debug");
+  const error: Debugger = log.extend("error");
   try {
     // For my development environment
     const agent = process.env.SOCKS_PROXY
@@ -14,10 +19,9 @@ const watchPrice = async (client: Client) => {
 
     const server = "wss://ws-feed.exchange.coinbase.com";
     const ws = new WebSocket(server, { agent });
-    console.log("Connecting...");
 
     ws.on("open", () => {
-      console.log(`Subsrcibe to server ${server}`);
+      log(`Subsrcibe to server ${server}`);
       const subscribe = JSON.stringify({
         type: "subscribe",
         product_ids: ["BTC-USD"],
@@ -26,25 +30,24 @@ const watchPrice = async (client: Client) => {
       ws.send(subscribe);
     });
 
-    ws.on("error", (error) => {
-      console.error("Error in watchPrice: ", error);
+    ws.on("error", (err) => {
+      error("Error in watchPrice: ", err);
     });
 
     ws.on("message", (message: string) => {
       const ticker = JSON.parse(message) as CoinbaseMessage;
-      if (process.env.NODE_ENV == "development")
-        console.debug(`Received message from server: ${message}`);
+      debug(`Received message from server: ${message}`);
       if (ticker.type === "ticker")
         client.updateTicker(+ticker.price, +ticker.open_24h);
     });
 
     ws.on("close", (message: string) => {
-      console.log(`Unsubscribe to server ${server} with message: ${message}`);
-      console.log(`Trying to reconnect in 5 seconds...`);
+      log(`Unsubscribe to server ${server} with message: ${message}`);
+      log(`Trying to reconnect in 5 seconds...`);
       setTimeout(() => watchPrice(client), 5000);
     });
-  } catch (error) {
-    console.error("Error in watchPrice: ", error);
+  } catch (err) {
+    error("Error in watchPrice: ", err);
   }
 };
 
